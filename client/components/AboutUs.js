353159a755353c2b80ffac2f8165ffd53cb1c49b
Milan Patel <milanpatel92@gmail.com>
2019-06-12 14:21:22 -0400

    Initial commit with boilerplat changes

---
+++ client/components/AboutUs.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function AboutUs() {
+  return (
+    <div>
+      <div className="intro">hey, i'm milan</div>
+      <div className="tagline">
+        welcome to my desk - a spot for all the scribbles that deserved not to
+        be lost.
+      </div>
+      <div className="tagline">Blog | Notes | Life | Area51</div>
+      <div className="icons-social">
+        <a target="_blank" href="https://twitter.com/pbmilan">
+          <i className="fab fa-twitter" />
+        </a>
+        <a target="_blank" href="https://github.com/milanhpatel">
+          <i className="fab fa-github" />
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/milanpatel92">
+          <i className="fab fa-linkedin" />
+        </a>
+
+        <div id="root" />
+      </div>
+      <div className="icons-social">
+        <p>
+          {' '}
+          Minim sint veniam pariatur laborum aliquip nisi ad exercitation
+          exercitation minim tempor minim. Exercitation et tempor sit anim ea ea
+          adipisicing nulla consectetur tempor. Voluptate in minim occaecat
+          deserunt. Mollit cillum laboris aute reprehenderit elit occaecat
+          proident qui fugiat non. Duis ex minim anim ut sunt id ea aliquip
+          dolore amet in. Commodo reprehenderit exercitation eiusmod in irure
+          consequat eiusmod eiusmod nostrud anim.
+        </p>
+      </div>
+    </div>
+  )
+}
--- client/components/index.js
+++ client/components/index.js
@@ -5,4 +5,5 @@
  */
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
+export {default as AboutUs} from './AboutUs'
 export {Login, Signup} from './auth-form'
